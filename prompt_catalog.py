c3a613edcb5e22a2147b3c50f257771bf941ffa9
toimi <toimi@rambler.ru>
2026-02-04 18:48:40 +0300

    Initial commit

--- prompt_catalog.py
+++ prompt_catalog.py
@@ -2,8 +2,7 @@ from typing import List, Tuple
 
 # ========= Anime set (from https://github.com/Atmyre/CASteer)  ==========
 
-ANIME_PROMPT = [
- ('goldfish, anime style', 'goldfish'),
+ANIME_PROMPT = [('goldfish, anime style', 'goldfish'),
  ('great white shark, anime style', 'great white shark'),
  ('tiger shark, anime style', 'tiger shark'),
  ('hammerhead, anime style', 'hammerhead'),
@@ -54,8 +53,30 @@ ANIME_PROMPT = [
  ('African crocodile, anime style', 'African crocodile')
 ] 
 
-MAIN_PROMPT =[('goldfish, anime style', 'goldfish'), ('great white shark, anime style', 'great white shark'), ('tiger shark, anime style', 'tiger shark'), ('hammerhead, anime style', 'hammerhead'), ('electric ray, anime style', 'electric ray'), ('stingray, anime style', 'stingray'), ('cock, anime style', 'cock'), ('hen, anime style', 'hen'), ('ostrich, anime style', 'ostrich'), ('brambling, anime style', 'brambling'), ('goldfinch, anime style', 'goldfinch'), ('house finch, anime style', 'house finch'), ('junco, anime style', 'junco'), ('indigo bunting, anime style', 'indigo bunting'), ('robin, anime style', 'robin'), ('bulbul, anime style', 'bulbul'), ('jay, anime style', 'jay'), ('magpie, anime style', 'magpie'), ('chickadee, anime style', 'chickadee'), ('water ouzel, anime style', 'water ouzel'), "('brown knife, anime style', 'brown knife')", "('blue donut, anime style', 'blue donut')", "('knife with fork, anime style', 'knife with fork')", "('big donut, anime style', 'big donut')", "('donut with apple, anime style', 'donut with apple')", "('beautiful cow, anime style', 'beautiful cow')", "('beautiful knife, anime style', 'beautiful knife')", "('orange cow, anime style', 'orange cow')", "('purple sandwich, anime style', 'purple sandwich')", "('little cow, anime style', 'little cow')", "('sandwich and butter, anime style', 'sandwich and butter')", "('delicious sandwich, anime style', 'delicious sandwich')"]
+# ============== Anime_classic set =================
 
+ANIME_CLASSIC_PROMPT = [('brave warrior, anime style', 'brave warrior, classic style'),
+ ('beautiful woman, anime style', 'beautiful woman, classic style'),
+ ('charming house, anime style', 'charming house, classic style'),
+ ('multicolored peacock, anime style', 'multicolored peacock, classic style'),
+ ('charming man, anime style', 'charming man, classic style'),
+ ('magic flower, anime style', 'magic flower, classic style'),
+ ('kind dog, anime style', 'kind dog, classic style'),
+ ('beautiful yacht, anime style', 'beautiful yacht, classic style'),
+ ('handsome man, anime style', 'handsome man, classic style'),
+ ('dalmatian dog, anime style', 'dalmatian dog, classic style'),
+ ('golden fish, anime style', 'golden fish, classic style'),
+ ('incomparable woman, anime style', 'incomparable woman, classic style'),
+ ('lovely girl, anime style', 'lovely girl, classic style'),
+ ('graceful swan, anime style', 'graceful swan classic style'),
+ ('siamese cat, anime style', 'siamese cat, classic style'),
+ ('sweet boy, anime style', 'sweet boy, classic style'),
+ ('magnificent cowboy, anime style', 'magnificent cowboy, classic style'),
+ ('beautiful horse, anime style', 'beautiful horse, classic style'),
+ ('beautiful castle, anime style', 'beautiful castle, classic style'),
+ ('wonderful princess, anime style', 'wonderful princess, classic style'),
+ ('luxury autocar, anime style', 'luxury autocar, classic style')
+]
 
 
 
--- prompt_dataset.py
+++ prompt_dataset.py
@@ -53,7 +53,39 @@ ANIME_PROMPT = [('goldfish, anime style', 'goldfish'),
 ('African crocodile, anime style', 'African crocodile')
 ]
 
-MAIN_PROMPT =[('goldfish, anime style', 'goldfish'), ('great white shark, anime style', 'great white shark'), ('tiger shark, anime style', 'tiger shark'), ('hammerhead, anime style', 'hammerhead'), ('electric ray, anime style', 'electric ray'), ('stingray, anime style', 'stingray'), ('cock, anime style', 'cock'), ('hen, anime style', 'hen'), ('ostrich, anime style', 'ostrich'), ('brambling, anime style', 'brambling'), ('goldfinch, anime style', 'goldfinch'), ('house finch, anime style', 'house finch'), ('junco, anime style', 'junco'), ('indigo bunting, anime style', 'indigo bunting'), ('robin, anime style', 'robin'), ('bulbul, anime style', 'bulbul'), ('jay, anime style', 'jay'), ('magpie, anime style', 'magpie'), ('chickadee, anime style', 'chickadee'), ('water ouzel, anime style', 'water ouzel'), ('brown knife, anime style', 'brown knife'), ('blue donut, anime style', 'blue donut'), ('knife with fork, anime style', 'knife with fork'), ('big donut, anime style', 'big donut'), ('donut with apple, anime style', 'donut with apple'), ('beautiful cow, anime style', 'beautiful cow'), ('beautiful knife, anime style', 'beautiful knife'), ('orange cow, anime style', 'orange cow'), ('purple sandwich, anime style', 'purple sandwich'), ('little cow, anime style', 'little cow'), ('sandwich and butter, anime style', 'sandwich and butter'), ('delicious sandwich, anime style', 'delicious sandwich')]
+MAIN_PROMPT = [('brave warrior, anime style', 'brave warrior, classic style'),
+ ('beautiful woman, anime style', 'beautiful woman, classic style'),
+ ('charming house, anime style', 'charming house, classic style'),
+ ('multicolored peacock, anime style', 'multicolored peacock, classic style'),
+ ('charming man, anime style', 'charming man, classic style'),
+ ('magic flower, anime style', 'magic flower, classic style'),
+ ('kind dog, anime style', 'kind dog, classic style'),
+ ('beautiful yacht, anime style', 'beautiful yacht, classic style'),
+ ('handsome man, anime style', 'handsome man, classic style'),
+ ('dalmatian dog, anime style', 'dalmatian dog, classic style'),
+ ('golden fish, anime style', 'golden fish, classic style'),
+ ('incomparable woman, anime style', 'incomparable woman, classic style'),
+ ('lovely girl, anime style', 'lovely girl, classic style'),
+ ('graceful swan, anime style', 'graceful swan classic style'),
+ ('siamese cat, anime style', 'siamese cat, classic style'),
+ ('sweet boy, anime style', 'sweet boy, classic style'),
+ ('magnificent cowboy, anime style', 'magnificent cowboy, classic style'),
+ ('beautiful horse, anime style', 'beautiful horse, classic style'),
+ ('beautiful castle, anime style', 'beautiful castle, classic style'),
+ ('wonderful princess, anime style', 'wonderful princess, classic style'),
+ ('brown knife, anime style', 'brown knife, classic style'),
+ ('blue donut, anime style', 'blue donut, classic style'),
+ ('knife with fork, anime style', 'knife with fork, classic style'),
+ ('big donut, anime style', 'big donut, classic style'),
+ ('donut with apple, anime style', 'donut with apple, classic style'),
+ ('beautiful cow, anime style', 'beautiful cow, classic style'),
+ ('beautiful knife, anime style', 'beautiful knife, classic style'),
+ ('orange cow, anime style', 'orange cow, classic style'),
+ ('purple sandwich, anime style', 'purple sandwich, classic style'),
+ ('little cow, anime style', 'little cow, classic style'),
+ ('sandwich and butter, anime style', 'sandwich and butter, classic style'),
+ ('delicious sandwich, anime style', 'delicious sandwich, classic style')
+]
 
 
 
